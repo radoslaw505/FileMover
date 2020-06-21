cc15e3d941ea6372a36a93ced910c4b2a1a6bcfd
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2020-06-21 21:24:03 +0200

    commit

--- file_mover.py
+++ file_mover.py
@@ -5,30 +5,44 @@ from os import listdir, path
 from os.path import isfile, join
 from time import sleep
 
-from properties import FROM_PATH, TO_PATH
+from properties import FROM_PATH, TO_PATH, ERROR_PATH, file_extensions
 
 
 class FilerMover():
 
-    def _init_(self):
-        pass
+    def __init__(self):
+        self.check_directory(TO_PATH)
+        self.check_directory(ERROR_PATH)
 
     def move_files(self):
         file_list = [f for f in listdir(FROM_PATH) if isfile(join(FROM_PATH, f))]
-        
         for file in file_list:
-            print('Moving {} to {} directory.'.format(file, TO_PATH))
-            os.rename(FROM_PATH + file, TO_PATH + file)
-            sleep(0.5)
-            if path.exists(TO_PATH + file):
-                print("{} moved succesfully.".format(file))
+            try:
+                if file.split(".")[1] in file_extensions:
+                    print('Moving {} to {} directory.'.format(file, TO_PATH))
+                    os.rename(FROM_PATH + file, TO_PATH + file)
+                    sleep(0.5)
+                    if path.exists(TO_PATH + file):
+                        print("{} moved succesfully.".format(file))
+                        sleep(0.5)
+                else:
+                    print('Files with this extension are not supported. {} has been moved to {} directory.'.format(file, ERROR_PATH))
+                    os.rename(FROM_PATH + file, ERROR_PATH + file + 'ExtensionError')
+                    sleep(0.5)
+            except IndexError:
+                print('Files without extension are not supported. {} has been moved to {} directory.'.format(file, ERROR_PATH))
+                os.rename(FROM_PATH + file, ERROR_PATH + file + 'ExtensionError')
                 sleep(0.5)
 
-    def do_something_with_file():
+    def do_something_with_file(self):
         # For eg change name
         # Read file and throw it to the output
         # Collect file and execute sql
         pass
+
+    def check_directory(self, path):
+        if not os.path.exists(path):
+            os.mkdir(path)
 
 if __name__ == "__main__":
     mover = FilerMover()
--- properties.py
+++ properties.py
@@ -1,2 +1,4 @@
 FROM_PATH = 'C:/Users/rados/projects/Python/FileMover/move_from/'
 TO_PATH = 'C:/Users/rados/projects/Python/FileMover/move_to/'
+ERROR_PATH = 'C:/Users/rados/projects/Python/FileMover/error/'
+file_extensions = ['txt', 'csv'] # List 
