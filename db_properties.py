10469ef8f726c8a53c37faee2b7f0377c0e39b2f
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2020-06-28 11:29:38 +0200

    commit

---
+++ db_properties.py
@@ -0,0 +1,6 @@
+user = 'your_username'
+passwd = 'your_password'
+host = 'your_host'
+database = 'your_database'
+mysql_query = "INSERT INTO employees2 (first_name, last_name) VALUES (%s, %s)"
+val_num = 2
